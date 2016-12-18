62613f1c8427b807a2cf17e6fe0d3d010b8f26d9
JeffCtor <jeffrey@Jeffrey-MBP.local>
2016-12-18 18:58:01 +0800

    code reconstruction

--- app/src/main/java/cc/metapro/openct/libsearch/BookDetailActivity.java
+++ app/src/main/java/cc/metapro/openct/libsearch/BookDetailActivity.java
@@ -2,18 +2,31 @@ package cc.metapro.openct.libsearch;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import cc.metapro.openct.R;
+import me.zhanghai.android.materialprogressbar.MaterialProgressBar;
 
 public class BookDetailActivity extends AppCompatActivity {
 
     private static String mTitle, mURL;
+
+    private WebView mWebView;
 
     public static void actionStart(Context context, String title, String url) {
         mTitle = title;
@@ -34,11 +47,68 @@ public class BookDetailActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         setTitle(mTitle);
 
-        WebView webView = (WebView) findViewById(R.id.book_detail_web);
-        webView.loadUrl(mURL);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-        webView.getSettings().setSupportZoom(true);
-        webView.getSettings().setUseWideViewPort(true);
+        setWebView();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_back);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mWebView.canGoBack()) {
+                    mWebView.goBack();
+                } else {
+                    Toast.makeText(BookDetailActivity.this, "不能再后退啦", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setWebView() {
+        mWebView = (WebView) findViewById(R.id.book_detail_web);
+
+        final MaterialProgressBar pb = (MaterialProgressBar) findViewById(R.id.book_detail_progress);
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                pb.setProgress(0);
+                pb.setVisibility(View.VISIBLE);
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress == 100) {
+                    pb.setVisibility(View.GONE);
+                } else {
+                    if (pb.getVisibility() == View.INVISIBLE) {
+                        pb.setVisibility(View.VISIBLE);
+                    }
+                    pb.setProgress(newProgress);
+                }
+            }
+        });
+
+        mWebView.loadUrl(mURL);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setUseWideViewPort(true);
     }
 }
